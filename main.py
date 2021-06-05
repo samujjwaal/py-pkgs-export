0ca3171f13b7fb387c81ef3bc67ba067be764eae
Samujjwaal Dey <deysamujjwal@gmail.com>
2021-06-05 17:45:37 -0500

    Extract env name and pkg channels

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ import shlex
 import subprocess
 
 
-def print_hi(name):
-	print(f'Hi, {name}')
+def execute(command: str) -> str:
+	cmd = shlex.split(command)
+	output = subprocess.run(cmd, capture_output=True, text=True).stdout
+	return str(output)
 
 
-def execute():
-	cmd = shlex.split('conda list')
-	print(cmd)
-	cli = subprocess.run(cmd, capture_output=True, text=True)
-
-	print(cli.stdout)
-	# print(cli.stdout.decode().split('\n'))
+def generate_env_dict(string):
+	env = {}
+	print(string)
+	# print(string.split('\n')[0].split('name: ')[1])
+	env['name'] = string.split('\n')[0].split('name: ')[1]
+	env['channels'] = []
+	for i in range(2, string.split('\n').index('dependencies:')):
+		print(string.split('\n')[i].split('- ')[-1])
+		env['channels'].append(string.split('\n')[i].split('- ')[-1])
+	# print(string.split('\n')[2].split('name: ')[1])
+	print(env)
+	pass
 
 
 if __name__ == '__main__':
-	# print_hi('PyCharm')
-	execute()
+	# print(execute('conda env export --no-builds').split('\n'))
+	generate_env_dict(execute('conda env export --no-builds'))
+# print(execute('conda update --all'))
