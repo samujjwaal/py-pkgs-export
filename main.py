dbf25e604f3fda2ee485576d1f8aa6e94ae171d7
Samujjwaal Dey <deysamujjwal@gmail.com>
2021-06-05 21:36:31 -0500

    Extract conda & pip pkgs

--- main.py
+++ main.py
@@ -9,18 +9,43 @@ def execute(command: str) -> str:
 	return str(output)
 
 
-def generate_env_dict(string):
+def extract_pip_packages(pkg_list, start, end):
+	pkgs = {}
+	for k in range(start, end):
+		pkg_name = pkg_list[k].split('- ')[1].split('==')[0]
+		pkg_version = pkg_list[k].split('- ')[1].split('==')[1]
+		print(pkg_name, pkg_version)
+		pkgs[pkg_name] = pkg_version
+	return pkgs
+
+
+def extract_conda_pkgs(pkg_list, start, end):
+	conda_pkgs = {}
+	pip_pkgs = {}
+	for j in range(start, end):
+		if (pkg := pkg_list[j].split('- ')[1]) != 'pip:':
+			pkg_name = pkg.split('=')[0]
+			pkg_version = pkg.split('=')[1]
+			conda_pkgs[pkg_name] = pkg_version
+		else:
+			pip_pkgs = extract_pip_packages(pkg_list, j + 1, end)
+			break
+
+	return conda_pkgs, pip_pkgs
+
+
+def generate_env_dict(cmd_output):
 	env = {}
-	print(string)
-	# print(string.split('\n')[0].split('name: ')[1])
-	env['name'] = string.split('\n')[0].split('name: ')[1]
+	print(cmd_output)
+	data = cmd_output.split('\n')
+	env['name'] = data[0].split('name: ')[1]
 	env['channels'] = []
-	for i in range(2, string.split('\n').index('dependencies:')):
-		print(string.split('\n')[i].split('- ')[-1])
-		env['channels'].append(string.split('\n')[i].split('- ')[-1])
-	# print(string.split('\n')[2].split('name: ')[1])
+	for i in range(2, data.index('dependencies:')):
+		env['channels'].append(data[i].split('- ')[1])
+
+	env['pkgs'], env['pip'] = extract_conda_pkgs(data, i + 2, len(data) - i)
+
 	print(env)
-	pass
 
 
 if __name__ == '__main__':
