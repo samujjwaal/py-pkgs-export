cd85739c2df888398443d566b92dcd5c46eba098
Samujjwaal Dey <deysamujjwal@gmail.com>
2021-06-07 02:44:34 -0500

    Add functions to generate env.yml & requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import shlex
 from subprocess import run
 
+import yaml
+
 
 def execute(command: str) -> str:
 	return str(run(shlex.split(command), capture_output=True, text=True).stdout)
@@ -16,7 +18,7 @@ def extract_pip_packages(pkg_list: list[str], start: int, end: int) -> dict:
 	return pkgs
 
 
-def extract_conda_pkgs(pkg_list: list[str], start: int, end: int) -> tuple[dict, dict]:
+def extract_pkgs(pkg_list: list[str], start: int, end: int) -> tuple[dict, dict]:
 	conda_pkgs = {}
 	pip_pkgs = {}
 	for j in range(start, end):
@@ -39,13 +41,46 @@ def generate_env_dict(cmd_output: str) -> dict:
 	env['channels'] = []
 	for i in range(2, data.index('dependencies:')):
 		env['channels'].append(data[i].split('- ')[1])
-	env['pkgs'], env['pip'] = extract_conda_pkgs(data, i + 2, len(data) - i)
+	# noinspection PyUnboundLocalVariable
+	env['conda'], env['pip'] = extract_pkgs(data, i + 2, len(data) - i)
 	env['prefix'] = data[-2].split(': ')[1]
-	print(env)
+	# print(env)
 	return env
 
 
+def generate_env_yml(config: dict, user_installs: tuple[str] = None) -> None:
+	if not user_installs:
+		conda_pkgs = [f"{k}={v}" for k, v in config['conda'].items()]
+	else:
+		conda_pkgs = [f"{k}={v}" for k, v in config['conda'].items() if k in user_installs]
+	pip_pkgs = [{'pip': [f"{k}=={v}" for k, v in config['pip'].items()]}]
+	# noinspection PyTypeChecker
+	conda_pkgs.extend(pip_pkgs)
+	print(conda_pkgs)
+	export = {'name': config['name'], 'channels': config['channels'], 'dependencies': conda_pkgs}
+
+	with open(f"{export['name']}.yml", 'w') as file:
+		yaml.dump(export, file, sort_keys=False)
+
+
+def from_history(c1, c2):
+	return tuple(c1['conda'].keys()) and list(c2['conda'].keys())
+
+
+def generate_req_file(config: dict, user_installs: tuple[str]):
+	conda_pkgs = [f"{k}=={v}\n" for k, v in config['conda'].items() if k in user_installs and k != 'python']
+	pip_pkgs = [f"{k}=={v}\n" for k, v in config['pip'].items()]
+
+	with open(f"requirements.txt", 'w') as file:
+		file.writelines(conda_pkgs)
+		file.writelines(pip_pkgs)
+
+
 if __name__ == '__main__':
-	# print(execute('conda env export --no-builds').split('\n'))
-	generate_env_dict(execute('conda env export --no-builds'))
-# print(execute('conda update --all'))
+	env_config = generate_env_dict(execute('conda env export --no-builds'))
+	env_config_history = generate_env_dict(execute('conda env export --from-history'))
+	print(env_config)
+	print(env_config_history)
+	# generate_env_yml(env_config, from_history(env_config,env_config_history))
+	# generate_env_yml(env_config, tuple('python'))
+	generate_req_file(env_config, from_history(env_config, env_config_history))
